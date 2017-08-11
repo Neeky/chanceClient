f905aa78fe0fc75da656e1f78b2de838cc04dbc6
Neeky <1721900707@qq.com>
2017-08-11 16:13:50 +0800

    17.08.11.04 cninfo 深市主板公司列表的解析

---
+++ chanceClient/spiders/cninfoSSZBCompanyListSpider.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class CompanyListItem(scrapy.Item):
+    stockCodeAndName=scrapy.Field()
+    infoPage =scrapy.Field()
+
+class CninfosszbcompanylistspiderSpider(scrapy.Spider):
+    name = 'cninfoSSZBCompanyListSpider'
+    allowed_domains = ['www.cninfo.com.cn']
+    start_urls = ['http://www.cninfo.com.cn/cninfo-new/information/companylist']
+
+    def parse(self, response):
+        #深市主板
+        sszbil=ItemLoader(item=CompanyListItem(),response=response)
+        sszbil.add_xpath('stockCodeAndName'    ,"//div[@id='con-a-1']/descendant::a/text()")
+        sszbil.add_xpath('infoPage'            ,"//div[@id='con-a-1']/descendant::a/@href")
+        return sszbil.load_item()
