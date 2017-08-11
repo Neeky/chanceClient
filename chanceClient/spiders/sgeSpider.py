93aafab4f61912243b1809720c4978d3d2678e9e
Neeky <1721900707@qq.com>
2017-08-11 14:03:15 +0800

    17.08.11.03 sge 001 已经完成页面的解析

---
+++ chanceClient/spiders/sgeSpider.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.loader import ItemLoader
+
+class GlodPriceItem(scrapy.Item):
+    contract   =scrapy.Field()
+    highest    =scrapy.Field()
+    lowest     =scrapy.Field()
+    opening    =scrapy.Field()
+    closing    =scrapy.Field()
+
+    def convert(self):
+        datas=dict(self)
+        res  ={}
+        res['contract'] =datas['contract'][0].strip() if datas['contract'][0] != None else 'Au99.99'
+        res['highest']  =float(datas['highest'][0])   if datas['highest'][0]  != None else -1
+        res['lowest']   =float(datas['lowest'][0])    if datas['lowest'][0]   != None else -1
+        res['opening']  =float(datas['opening'][0])   if datas['opening'][0]  != None else -1
+        res['closing']  =float(datas['closing'][0])   if datas['closing'][0]  != None else -1
+        return res
+
+class SgespiderSpider(scrapy.Spider):
+    name = 'sgeSpider'
+    allowed_domains = ['www.sge.com.cn']
+    start_urls = ['https://www.sge.com.cn/hqsj']
+    def parse(self, response):
+        gpil=ItemLoader(item=GlodPriceItem(),response=response)
+        auXpath='//div/descendant::table[position()=1]/descendant::tr[position()=1]' 
+        gpil.add_xpath('contract',auXpath +'//td[position()=1]/text()')
+        gpil.add_xpath('opening' ,auXpath +'//td[position()=2]/text()')
+        gpil.add_xpath('closing' ,auXpath +'//td[position()=3]/text()')
+        gpil.add_xpath('highest' ,auXpath +'//td[position()=4]/text()')
+        gpil.add_xpath('lowest'  ,auXpath +'//td[position()=5]/text()')
+        return gpil.load_item()
