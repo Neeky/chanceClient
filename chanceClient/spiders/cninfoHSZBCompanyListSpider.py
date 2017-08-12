ac99a722c53ef629c49bf854b50436dfa9cd11a1
Neeky <1721900707@qq.com>
2017-08-12 09:46:11 +0800

    17.08.12.01 cninfo 沪市主板

---
+++ chanceClient/spiders/cninfoHSZBCompanyListSpider.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.loader import ItemLoader
+from .cninfo import CompanyListItem
+
+class CninfohszbcompanylistspiderSpider(scrapy.Spider):
+    name = 'cninfoHSZBCompanyListSpider'
+    allowed_domains = ['www.cninfo.com.cn']
+    start_urls = ['http://www.cninfo.com.cn/cninfo-new/information/companylist']
+
+    def parse(self, response):
+        #沪市主板
+        hszbil=ItemLoader(item=CompanyListItem(),response=response)
+        hszbil.add_xpath('stockCodeAndName',"//div[@id='con-a-4']/descendant::a/text()")
+        hszbil.add_xpath('infoPage'        ,"//div[@id='con-a-4']/descendant::a/@href")
+        return hszbil.load_item()
