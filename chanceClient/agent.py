216d43266f29f7f1320e0eae85149b5023c5fe8b
Neeky <1721900707@qq.com>
2017-08-12 14:21:17 +0800

    17.08.12.03 shibor-agent 增加把数据post的Server的逻辑

---
+++ chanceClient/agent.py
@@ -0,0 +1,37 @@
+# * --encoding-- utf8
+import requests
+from .spiders.cninfo import CompanyListItem
+from .spiders.shiborSpider import ShiborRateItem
+
+
+class Agent(object):
+    "代理用来对已经爬下来的数据进行处理、如发送到server Agent作为所有其它代理的基类"
+    item=None
+    server="http://www.cstudio.com/component/"
+    api=""
+    def __init__(self,item):
+        self.item=item
+
+    @property
+    def ajaxaddress(self):
+        return "{0}{1}".format(self.server,self.api)
+
+    def postToServer(self):
+        pass
+
+
+class ShiborAgent(Agent):
+    api="shibor/add"
+    def postToServer(self):
+        url=self.ajaxaddress
+        requests.post(url,data=self.item.convert())
+
+
+
+def agentRouter(item):
+    """
+    一个Agent的分发器，不同的的Item分给不同的Agent
+    """
+    if isinstance(item,ShiborRateItem):
+        sa=ShiborAgent(item)
+        sa.postToServer()
--- chanceClient/pipelines.py
+++ chanceClient/pipelines.py
@@ -5,12 +5,14 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from .spiders.cninfo import CompanyListItem
- 
+from .spiders.shiborSpider import ShiborRateItem 
+from .agent import agentRouter
 
 class ChanceclientPipeline(object):
     def process_item(self, item, spider):
         print('--'*24)
-        print([x for x in item.convert()])
+        print(item.convert())
         #print(isinstance(item,CompanyListItem))
         print('--'*24)
+        agentRouter(item)
         return item
